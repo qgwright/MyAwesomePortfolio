a3336e73b564ffcd3f9e95442691caa7ae1e16f0
qgwright <qgwright@hotmail.com>
2019-03-28 17:53:45 -0400

    update

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+	## CONFIG ##
+
+	# ENTER YOUR EMAIL ADDRESS BELOW TO RECEIVE MESSAGES VIA CONTACT FORM
+	$recipient = "contact@example.com";
+
+	# ENTER THE SUBJECT FOR EMAILS
+	$subject = "Contact form submission";
+
+	## FORM VALUES ##
+	$sender = $recipient;
+
+	# MAIL BODY
+	$body .= "Name: ".$_REQUEST['name']." \n";
+	$body .= "Email: ".$_REQUEST['email']." \n";
+	$body .= "Message: ".$_REQUEST['message']." \n";
+
+	## SEND MESSGAE ##
+
+	mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
+
+?>
